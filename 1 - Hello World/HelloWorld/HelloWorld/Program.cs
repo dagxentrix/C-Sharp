7c98d28f2a9fc58cf40c1f15ebb6e719b627e2c4
dagxentrix <dagpedersen92@hotmail.com>
2018-01-02 21:51:13 +0100

    1 og 2

---
+++ 1 - Hello World/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+            Console.WriteLine("Hva heter du?");
+
+            string input = Console.ReadLine();
+            if (input == "Dag")
+            {
+                Console.WriteLine("Dag er best");
+            }
+            else
+            {
+                Console.WriteLine("Hei " + input);
+            }            
+        }
+        
+    }
+}
+
---
+++ 2 - Variabler og konstanter/Variables/Variables/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var tall = "1234";
+                byte b = Convert.ToByte(tall);
+                Console.WriteLine(b);
+
+                string str = "true";
+                bool bol = Convert.ToBoolean(str);
+                Console.WriteLine(bol);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Tallet kan ikke bli konvertert til byte");
+            }
+            byte number = 2;
+            int count = int.MaxValue;
+            string firstName = "Dag";
+            bool isWorking = false;
+            Console.WriteLine(number + " " + count + " " + firstName);
+            if (isWorking == true)
+            {
+                Console.WriteLine("Funksjer");
+            }
+            else
+            {
+                Console.WriteLine("Funkjer ikkje");
+            }
+        }
+    }
+}
