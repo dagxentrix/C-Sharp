97a237904f401efa5ecc121820092826a4fad099
dagxentrix <dagpedersen92@hotmail.com>
2018-01-03 16:01:50 +0100

    3

---
+++ 3 - Class/Classes/Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes.Math;
+
+namespace Classes
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person dag = new Person();
+            dag.FirstName = "Dag";
+            dag.LastName = "Pedersen";
+            dag.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
