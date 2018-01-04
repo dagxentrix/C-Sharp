b73709f2e260549be35328e9d2d81ba8e73f2c1f
dagxentrix <dagpedersen92@hotmail.com>
2018-01-04 20:54:53 +0100

    4,5,6

---
+++ 4 - Arrays/Arrays/Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+            numbers[2] = 3;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            bool []flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            string[] names = new string[3] { "Ein", "Två", "Tri" };
+            Console.WriteLine(names[0] + names[1] + names[2]);
+
+
+        }
+    }
+}
---
+++ 5 - Strings/Strings/Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Dag";
+            var lastName = "Pedersen";
+
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format("Jeg heter {0} {1}", firstName, lastName);
+
+            Console.WriteLine("fullName =" + fullName);
+            Console.WriteLine("myFullName =" + myFullName);
+
+            var names = new string[3] { "Dag", "Dagny", "Dagrun" };
+            var formattedNames = string.Join(",", names);
+
+            Console.WriteLine(formattedNames);
+
+            var myText = "Hey Dagny\n se på denne stien\n c:\\mappe1\\mappe2\\mappe3";
+            var text = @"Hey Dag
+Se på denne stien
+c:\mappe1\mappe2\mappe3";
+            Console.WriteLine(myText);
+            Console.WriteLine(text);
+        }
+    }
+}
