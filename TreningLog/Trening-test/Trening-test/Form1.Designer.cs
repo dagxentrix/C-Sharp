9ed8029a72aef707835a652f88fab4fb881335c4
dagxentrix <dagpedersen92@hotmail.com>
2018-01-22 07:38:04 +0100

    Oppdatert

--- TreningLog/Trening-test/Trening-test/Form1.Designer.cs
+++ TreningLog/Trening-test/Trening-test/Form1.Designer.cs
@@ -28,27 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lvlOvelser = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
             this.listOvelser = new System.Windows.Forms.ListBox();
             this.listTreningData = new System.Windows.Forms.ListBox();
-            this.lvlTreningData = new System.Windows.Forms.Label();
             this.btnLeggTilData = new System.Windows.Forms.Button();
             this.txtVekt = new System.Windows.Forms.TextBox();
             this.txtRep = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.btnShowHide = new System.Windows.Forms.Button();
             this.txtOvelseNavn = new System.Windows.Forms.TextBox();
             this.btnNyOvelse = new System.Windows.Forms.Button();
+            this.lblNavnOvelse = new System.Windows.Forms.Label();
+            this.lblVekt = new System.Windows.Forms.Label();
+            this.lblRep = new System.Windows.Forms.Label();
+            this.btnSlettOvelse = new System.Windows.Forms.Button();
+            this.btnSlettData = new System.Windows.Forms.Button();
+            this.btnShowHideData = new System.Windows.Forms.Button();
+            this.txtPerson = new System.Windows.Forms.TextBox();
+            this.lblPerson = new System.Windows.Forms.Label();
+            this.comboboxNavn = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // lvlOvelser
-            // 
-            this.lvlOvelser.AutoSize = true;
-            this.lvlOvelser.Location = new System.Drawing.Point(9, 40);
-            this.lvlOvelser.Name = "lvlOvelser";
-            this.lvlOvelser.Size = new System.Drawing.Size(43, 13);
-            this.lvlOvelser.TabIndex = 0;
-            this.lvlOvelser.Text = "Øvelser";
             // 
             // listOvelser
             // 
@@ -68,18 +66,9 @@
             this.listTreningData.TabIndex = 3;
             this.listTreningData.SelectedIndexChanged += new System.EventHandler(this.listTreningData_SelectedIndexChanged);
             // 
-            // lvlTreningData
-            // 
-            this.lvlTreningData.AutoSize = true;
-            this.lvlTreningData.Location = new System.Drawing.Point(324, 40);
-            this.lvlTreningData.Name = "lvlTreningData";
-            this.lvlTreningData.Size = new System.Drawing.Size(48, 13);
-            this.lvlTreningData.TabIndex = 2;
-            this.lvlTreningData.Text = "Historikk";
-            // 
             // btnLeggTilData
             // 
-            this.btnLeggTilData.Location = new System.Drawing.Point(563, 164);
+            this.btnLeggTilData.Location = new System.Drawing.Point(563, 208);
             this.btnLeggTilData.Name = "btnLeggTilData";
             this.btnLeggTilData.Size = new System.Drawing.Size(75, 23);
             this.btnLeggTilData.TabIndex = 4;
@@ -96,26 +85,20 @@
             // 
             // txtRep
             // 
-            this.txtRep.Location = new System.Drawing.Point(563, 111);
+            this.txtRep.Location = new System.Drawing.Point(563, 127);
             this.txtRep.Name = "txtRep";
             this.txtRep.Size = new System.Drawing.Size(100, 20);
             this.txtRep.TabIndex = 6;
             // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(563, 138);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(72, 20);
-            this.dateTimePicker1.TabIndex = 8;
-            // 
             // btnShowHide
             // 
-            this.btnShowHide.Location = new System.Drawing.Point(12, 365);
+            this.btnShowHide.Location = new System.Drawing.Point(179, 286);
             this.btnShowHide.Name = "btnShowHide";
             this.btnShowHide.Size = new System.Drawing.Size(75, 23);
             this.btnShowHide.TabIndex = 9;
             this.btnShowHide.Text = "Vis valg";
             this.btnShowHide.UseVisualStyleBackColor = true;
+            this.btnShowHide.Click += new System.EventHandler(this.btnShowHide_Click);
             // 
             // txtOvelseNavn
             // 
@@ -126,49 +109,157 @@
             // 
             // btnNyOvelse
             // 
-            this.btnNyOvelse.Location = new System.Drawing.Point(179, 164);
+            this.btnNyOvelse.Location = new System.Drawing.Point(179, 127);
             this.btnNyOvelse.Name = "btnNyOvelse";
             this.btnNyOvelse.Size = new System.Drawing.Size(75, 23);
             this.btnNyOvelse.TabIndex = 11;
             this.btnNyOvelse.Text = "Legg til øvelse";
             this.btnNyOvelse.UseVisualStyleBackColor = true;
+            this.btnNyOvelse.Click += new System.EventHandler(this.btnNyOvelse_Click);
+            // 
+            // lblNavnOvelse
+            // 
+            this.lblNavnOvelse.AutoSize = true;
+            this.lblNavnOvelse.BackColor = System.Drawing.Color.Transparent;
+            this.lblNavnOvelse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblNavnOvelse.Location = new System.Drawing.Point(179, 65);
+            this.lblNavnOvelse.Name = "lblNavnOvelse";
+            this.lblNavnOvelse.Size = new System.Drawing.Size(106, 17);
+            this.lblNavnOvelse.TabIndex = 12;
+            this.lblNavnOvelse.Text = "Navn på øvelse";
+            // 
+            // lblVekt
+            // 
+            this.lblVekt.AutoSize = true;
+            this.lblVekt.BackColor = System.Drawing.Color.Transparent;
+            this.lblVekt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblVekt.Location = new System.Drawing.Point(563, 65);
+            this.lblVekt.Name = "lblVekt";
+            this.lblVekt.Size = new System.Drawing.Size(36, 17);
+            this.lblVekt.TabIndex = 13;
+            this.lblVekt.Text = "Vekt";
+            // 
+            // lblRep
+            // 
+            this.lblRep.AutoSize = true;
+            this.lblRep.BackColor = System.Drawing.Color.Transparent;
+            this.lblRep.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblRep.Location = new System.Drawing.Point(563, 111);
+            this.lblRep.Name = "lblRep";
+            this.lblRep.Size = new System.Drawing.Size(75, 17);
+            this.lblRep.TabIndex = 14;
+            this.lblRep.Text = "Repetisjon";
+            // 
+            // btnSlettOvelse
+            // 
+            this.btnSlettOvelse.Location = new System.Drawing.Point(179, 166);
+            this.btnSlettOvelse.Name = "btnSlettOvelse";
+            this.btnSlettOvelse.Size = new System.Drawing.Size(75, 23);
+            this.btnSlettOvelse.TabIndex = 16;
+            this.btnSlettOvelse.Text = "Slett Øvelse";
+            this.btnSlettOvelse.UseVisualStyleBackColor = true;
+            this.btnSlettOvelse.Click += new System.EventHandler(this.btnSlettOvelse_Click);
+            // 
+            // btnSlettData
+            // 
+            this.btnSlettData.Location = new System.Drawing.Point(563, 237);
+            this.btnSlettData.Name = "btnSlettData";
+            this.btnSlettData.Size = new System.Drawing.Size(75, 23);
+            this.btnSlettData.TabIndex = 17;
+            this.btnSlettData.Text = "Slett data";
+            this.btnSlettData.UseVisualStyleBackColor = true;
+            this.btnSlettData.Click += new System.EventHandler(this.btnSlettData_Click);
+            // 
+            // btnShowHideData
+            // 
+            this.btnShowHideData.Location = new System.Drawing.Point(563, 286);
+            this.btnShowHideData.Name = "btnShowHideData";
+            this.btnShowHideData.Size = new System.Drawing.Size(75, 23);
+            this.btnShowHideData.TabIndex = 18;
+            this.btnShowHideData.Text = "Vis valg";
+            this.btnShowHideData.UseVisualStyleBackColor = true;
+            this.btnShowHideData.Click += new System.EventHandler(this.btnShowHideData_Click);
+            // 
+            // txtPerson
+            // 
+            this.txtPerson.Location = new System.Drawing.Point(563, 390);
+            this.txtPerson.Name = "txtPerson";
+            this.txtPerson.Size = new System.Drawing.Size(100, 20);
+            this.txtPerson.TabIndex = 19;
+            // 
+            // lblPerson
+            // 
+            this.lblPerson.AutoSize = true;
+            this.lblPerson.BackColor = System.Drawing.Color.Transparent;
+            this.lblPerson.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblPerson.Location = new System.Drawing.Point(560, 150);
+            this.lblPerson.Name = "lblPerson";
+            this.lblPerson.Size = new System.Drawing.Size(53, 17);
+            this.lblPerson.TabIndex = 20;
+            this.lblPerson.Text = "Person";
+            // 
+            // comboboxNavn
+            // 
+            this.comboboxNavn.FormattingEnabled = true;
+            this.comboboxNavn.Items.AddRange(new object[] {
+            "Frida",
+            "Dag"});
+            this.comboboxNavn.Location = new System.Drawing.Point(563, 168);
+            this.comboboxNavn.Name = "comboboxNavn";
+            this.comboboxNavn.Size = new System.Drawing.Size(100, 21);
+            this.comboboxNavn.TabIndex = 22;
+            this.comboboxNavn.SelectedIndexChanged += new System.EventHandler(this.comboboxNavn_SelectedIndexChanged);
             // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(702, 515);
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = global::Trening_test.Properties.Resources.bilde;
+            this.ClientSize = new System.Drawing.Size(759, 466);
+            this.Controls.Add(this.comboboxNavn);
+            this.Controls.Add(this.lblPerson);
+            this.Controls.Add(this.txtPerson);
+            this.Controls.Add(this.btnShowHideData);
+            this.Controls.Add(this.btnSlettData);
+            this.Controls.Add(this.btnSlettOvelse);
+            this.Controls.Add(this.lblRep);
+            this.Controls.Add(this.lblVekt);
+            this.Controls.Add(this.lblNavnOvelse);
             this.Controls.Add(this.btnNyOvelse);
             this.Controls.Add(this.txtOvelseNavn);
             this.Controls.Add(this.btnShowHide);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.txtRep);
             this.Controls.Add(this.txtVekt);
             this.Controls.Add(this.btnLeggTilData);
             this.Controls.Add(this.listTreningData);
-            this.Controls.Add(this.lvlTreningData);
             this.Controls.Add(this.listOvelser);
-            this.Controls.Add(this.lvlOvelser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "formMain";
-            this.Text = "Trening";
+            this.Load += new System.EventHandler(this.formMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lvlOvelser;
         private System.Windows.Forms.ListBox listOvelser;
         private System.Windows.Forms.ListBox listTreningData;
-        private System.Windows.Forms.Label lvlTreningData;
         private System.Windows.Forms.Button btnLeggTilData;
         private System.Windows.Forms.TextBox txtVekt;
         private System.Windows.Forms.TextBox txtRep;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button btnShowHide;
         private System.Windows.Forms.TextBox txtOvelseNavn;
         private System.Windows.Forms.Button btnNyOvelse;
+        private System.Windows.Forms.Label lblNavnOvelse;
+        private System.Windows.Forms.Label lblVekt;
+        private System.Windows.Forms.Label lblRep;
+        private System.Windows.Forms.Button btnSlettOvelse;
+        private System.Windows.Forms.Button btnSlettData;
+        private System.Windows.Forms.Button btnShowHideData;
+        private System.Windows.Forms.TextBox txtPerson;
+        private System.Windows.Forms.Label lblPerson;
+        private System.Windows.Forms.ComboBox comboboxNavn;
     }
 }
 
--- TreningLog/Trening-test/Trening-test/Form1.cs
+++ TreningLog/Trening-test/Trening-test/Form1.cs
@@ -14,15 +14,19 @@ namespace Trening_test
 {
     public partial class formMain : Form
     {
+
         SqlConnection connection;
         string connectionString;
-
 
         public formMain()
         {
             InitializeComponent();
             connectionString = ConfigurationManager.ConnectionStrings["Trening_test.Properties.Settings.TreningConnectionString"].ConnectionString;
             PopulateOvelser();
+            PopulateTreningData();
+            hideData();
+            hideOvelse();
+            
         }
         //Henter ut alle øvelsene
         private void PopulateOvelser()
@@ -32,7 +36,6 @@ namespace Trening_test
             {
                 DataTable ovelseTable = new DataTable();
                 adapter.Fill(ovelseTable);
-
                 listOvelser.DisplayMember = "Navn";
                 listOvelser.ValueMember = "Id";
                 listOvelser.DataSource = ovelseTable;
@@ -41,9 +44,29 @@ namespace Trening_test
         //Henter ut TreningData ut i fra hvilke øvelse som er valgt
         private void PopulateTreningData()
         {
-            string query = "SELECT a.Vekt FROM TreningData a " +
+            string query = "SELECT * FROM TreningData a " +
                 "INNER JOIN ØvelseTreningData b ON a.Id = b.TreningDataId " +
-                "WHERE b.ØvelseId = @ØvelseId";
+                "WHERE b.ØvelseId = @ØvelseId " +
+                "ORDER BY Vekt DESC";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@ØvelseId", listOvelser.SelectedValue);
+                DataTable treningDataTable = new DataTable();
+                
+                adapter.Fill(treningDataTable);
+
+                treningDataTable.Columns.Add("Multi", typeof(string), "Vekt + ' kilo ' +Reps + ' reps' + ' ' +Person");
+                listTreningData.DisplayMember = "Multi";
+                listTreningData.ValueMember = "Id";
+                listTreningData.DataSource = treningDataTable;
+            }
+        }
+        private void btnSlettOvelse_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM Øvelse WHERE id = @ØvelseId";
             using (connection = new SqlConnection(connectionString))
             using (SqlCommand command = new SqlCommand(query, connection))
             using (SqlDataAdapter adapter = new SqlDataAdapter(command))
@@ -51,35 +74,58 @@ namespace Trening_test
                 command.Parameters.AddWithValue("@ØvelseId", listOvelser.SelectedValue);
                 DataTable treningDataTable = new DataTable();
                 adapter.Fill(treningDataTable);
-
-                listTreningData.DisplayMember = "Vekt";
-                listTreningData.ValueMember = "Id";
-                listTreningData.DataSource = treningDataTable;
             }
+            PopulateOvelser();
         }
-
-        private void listOvelser_SelectedIndexChanged(object sender, EventArgs e)
+        private void btnSlettData_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(listOvelser.SelectedValue.ToString());
+            string query = "DELETE FROM ØvelseTreningData WHERE TreningDataId = @TreningDataId;" +
+                "DELETE FROM TreningData WHERE id=@TreningDataId";
+            //string query = "DELETE FROM TreningData WHERE id = @TreningDataId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@TreningDataId", listTreningData.SelectedValue);
+                DataTable treningDataTable = new DataTable();
+                adapter.Fill(treningDataTable);
+            }
             PopulateTreningData();
         }
-
+        private void listOvelser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateTreningData();
+        }
         private void listTreningData_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            //MessageBox.Show(listTreningData.SelectedValue.ToString());
+        }
+        private void btnNyOvelse_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Øvelse VALUES(@OvelseNavn)";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@OvelseNavn", txtOvelseNavn.Text);
+                command.ExecuteScalar();
+                connection.Close();
+            }
+            PopulateOvelser();
         }
         //Legg til TreningData, henter ut Id og får koblet det til valgt øvelse
         private void btnLeggTilData_Click(object sender, EventArgs e)
         {
             int treningID = 0;
-            string query = "INSERT INTO TreningData VALUES(@TreningDataVekt, @TreningDataRep, @Dato)";
+            string query = "INSERT INTO TreningData VALUES(@TreningDataVekt, @TreningDataRep, @Dato, @Person)";
             using (connection = new SqlConnection(connectionString))
             using (SqlCommand command = new SqlCommand(query, connection))
             {
                 connection.Open();
                 command.Parameters.AddWithValue("@TreningDataVekt", txtVekt.Text);
-                command.Parameters.AddWithValue("@TreningDataRep", txtVekt.Text);
-                command.Parameters.AddWithValue("@Dato", dateTimePicker1.Value);
+                command.Parameters.AddWithValue("@TreningDataRep", txtRep.Text);
+                command.Parameters.AddWithValue("@Dato", DateTime.Today);
+                command.Parameters.AddWithValue("@Person", txtPerson.Text);
                 command.ExecuteScalar();
                 connection.Close();
 
@@ -105,6 +151,90 @@ namespace Trening_test
                 command.ExecuteScalar();
             }
             PopulateTreningData();
+        }
+        //Gjemmer labels, tekstbokser og knapper for å editere / legge til valg. 
+        private void hideOvelse()
+        {
+            btnNyOvelse.Visible = false;
+            txtOvelseNavn.Visible = false;           
+            lblNavnOvelse.Visible = false;            
+            btnSlettOvelse.Visible = false;
+        }
+        private void hideData()
+        {
+            txtRep.Visible = false;
+            txtVekt.Visible = false;
+            btnLeggTilData.Visible = false;
+            lblRep.Visible = false;
+            lblVekt.Visible = false;
+            btnSlettData.Visible = false;
+            lblPerson.Visible = false;
+            txtPerson.Visible = false;
+        }
+        //Viser labels, tekstbokser og knapper for å editere / legge til valg. 
+        private void showOvelse()
+        {
+            btnNyOvelse.Visible = true;
+            txtOvelseNavn.Visible = true;
+            lblNavnOvelse.Visible = true;
+            btnSlettOvelse.Visible = true;
+        }
+        private void showData()
+        {
+            txtRep.Visible = true;
+            txtVekt.Visible = true;
+            //datePicker.Visible = true;
+            //lblDato.Visible = true;
+            btnLeggTilData.Visible = true;
+            lblVekt.Visible = true;
+            lblRep.Visible = true;  
+            btnSlettData.Visible = true;
+            lblPerson.Visible = true;
+            //txtPerson.Visible = true;
+        }
+        private void btnShowHide_Click(object sender, EventArgs e)
+        {
+            if (btnNyOvelse.Visible == false)
+            {
+                showOvelse();
+                btnShowHide.Text = "Skjul valg";
+            }
+            else
+            {
+                hideOvelse();
+                btnShowHide.Text = "Vis valg";
+            }
+        }
+
+        private void btnShowHideData_Click(object sender, EventArgs e)
+        {
+            if (btnLeggTilData.Visible == false)
+            {
+                showData();
+                btnShowHideData.Text = "Skjul valg";
+            }
+            else
+            {
+                hideData();
+                btnShowHideData.Text = "Vis valg";
+            }
+
+        }
+        private void comboboxNavn_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboboxNavn.SelectedIndex == 0)
+            {
+                txtPerson.Text = "Frida";
+            }
+            else if(comboboxNavn.SelectedIndex == 1)
+            {
+                txtPerson.Text = "Dag";
+            }
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
